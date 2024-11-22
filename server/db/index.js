8dc5e721e1119ce4706aa65b5e72197cf1270e34
ojas-17 <ojasbalsaraf@gmail.com>
2024-11-22 20:15:53 +0530

    major project

--- server/db/index.js
+++ server/db/index.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
         console.log(`MONGODB connected! Host: ${connectionInstance.connection.host}`);
     } catch (error) {
+        console.log(`MONGO DB URL: ${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
         console.log("MONGODB connection failed:", error);
         process.exit(1);
     }
